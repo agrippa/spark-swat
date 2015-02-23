5c6aa5fe239c990902539f24c9f09638c69a50ec
Max Grossman <jmaxg3@gmail.com>
2015-02-22 18:12:29 -0600

    fixes to run with CLWrapper

--- spark-example-cl/run.sh
+++ spark-example-cl/run.sh
@@ -6,6 +6,7 @@ if [[ $# != 1 ]]; then
 fi
 
 spark-submit --class SparkKMeans \
+        --jars ${SWAT_HOME}/swat/target/swat-1.0-SNAPSHOT.jar \
         --master spark://localhost:7077 \
-        ${SWAT_HOME}/spark-example/target/sparkkmeans-0.0.0.jar \
+        ${SWAT_HOME}/spark-example-cl/target/sparkkmeans-0.0.0.jar \
         run 3 $1 hdfs://$(hostname):54310/converted
