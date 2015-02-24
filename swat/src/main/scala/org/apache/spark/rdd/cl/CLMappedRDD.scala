52df683fd92882dff055652f9ca3ec3967e138d6
Max Grossman <jmaxg3@gmail.com>
2015-02-23 20:25:35 -0600

    work on mapper

--- swat/src/main/scala/org/apache/spark/rdd/cl/CLMappedRDD.scala
+++ swat/src/main/scala/org/apache/spark/rdd/cl/CLMappedRDD.scala
@@ -2,8 +2,16 @@ package org.apache.spark.rdd.cl
 
 import scala.reflect.ClassTag
 
+import java.util.LinkedList
+
 import org.apache.spark.{Partition, TaskContext}
 import org.apache.spark.rdd._
+
+import com.amd.aparapi.internal.model.ClassModel
+import com.amd.aparapi.internal.model.Entrypoint
+import com.amd.aparapi.internal.writer.KernelWriter
+import com.amd.aparapi.internal.writer.BlockWriter.ScalaParameter
+import com.amd.aparapi.internal.writer.BlockWriter.ScalaParameter.DIRECTION
 
 class CLMappedRDD[U: ClassTag, T: ClassTag](prev: RDD[T], f: T => U)
   extends RDD[U](prev) {
@@ -11,18 +19,31 @@ class CLMappedRDD[U: ClassTag, T: ClassTag](prev: RDD[T], f: T => U)
   override def getPartitions: Array[Partition] = firstParent[T].partitions
 
   override def compute(split: Partition, context: TaskContext) = {
+    System.setProperty("com.amd.aparapi.enable.INVOKEINTERFACE", "true")
+
+    val N = 1024
+    val acc : Array[T] = new Array[T](N)
+    val output : Array[U] = new Array[U](N)
+
+    val classModel : ClassModel = ClassModel.createClassModel(f.getClass)
+    val entryPoint : Entrypoint = classModel.getEntrypoint("apply", "(D)D", f); 
+
+    val params = new LinkedList[ScalaParameter]()
+    params.add(new ScalaParameter("double*", "v", DIRECTION.IN))
+    params.add(new ScalaParameter("double*", "out", DIRECTION.OUT))
+
+    val openCL : String = KernelWriter.writeToString(entryPoint, params)
+    System.err.println("OPENCL:\n" + openCL);
+
     val iter = new Iterator[U] {
       val nested = firstParent[T].iterator(split, context)
 
       var index = 0
       var nLoaded = 0
-      val N = 1024
-      val acc : Array[T] = new Array[T](N)
 
       def next() : U = {
         if (index >= nLoaded) {
           assert(nested.hasNext)
-          System.err.println("Reading in " + N + " inputs")
 
           index = 0
           nLoaded = 0
@@ -30,11 +51,15 @@ class CLMappedRDD[U: ClassTag, T: ClassTag](prev: RDD[T], f: T => U)
             acc(nLoaded) = nested.next
             nLoaded = nLoaded + 1
           }
+
+          for (i <- 0 until nLoaded) {
+            output(i) = f(acc(i))
+          }
         }
 
         val curr = index
         index = index + 1
-        f(acc(curr))
+        output(curr)
       }
 
       def hasNext : Boolean = {
