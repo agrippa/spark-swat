32bba12e3ea881b6a43997e70b15f6f91f8b689d
Max Grossman <jmaxg3@gmail.com>
2015-03-17 08:28:02 -0500

    convert the spark kmeans example to the point representation that works

--- spark-example-cl/src/main/scala/sparkkmeans/SparkKMeans.scala
+++ spark-example-cl/src/main/scala/sparkkmeans/SparkKMeans.scala
@@ -7,28 +7,11 @@ import scala.math._
 import org.apache.spark.rdd._
 import java.net._
 
-class Point(x:Double, y:Double, z:Double) extends java.io.Externalizable {
-    private var _x = x
-    private var _y = y
-    private var _z = z
-
-    def this() = this(0.0, 0.0, 0.0)
-
-    def get_x : Double = return _x
-    def get_y : Double = return _y
-    def get_z : Double = return _z
-
-    def readExternal(in:java.io.ObjectInput) {
-        _x = in.readDouble
-        _y = in.readDouble
-        _z = in.readDouble
-    }
-
-    def writeExternal(out:java.io.ObjectOutput) {
-        out.writeDouble(_x)
-        out.writeDouble(_y)
-        out.writeDouble(_z)
-    }
+class Point(val x: Float, val y: Float, val z: Float)
+    extends java.io.Serializable {
+  def this() {
+    this(0.0f, 0.0f, 0.0f)
+  }
 }
 
 object SparkKMeans {
@@ -83,21 +66,21 @@ object SparkKMeans {
         for (iter <- 0 until iters) {
             val classified = points.map(point => classify(point, centers))
             val counts = classified.countByKey()
-            val sums = classified.reduceByKey((a, b) => new Point(a.get_x + b.get_x,
-                    a.get_y + b.get_y, a.get_z + b.get_z))
+            val sums = classified.reduceByKey((a, b) => new Point(a.x + b.x,
+                    a.y + b.y, a.z + b.z))
             val averages = sums.map(kv => {
                 val cluster_index:Int = kv._1;
                 val p:Point = kv._2;
-                (cluster_index, new Point(p.get_x / counts(cluster_index),
-                    p.get_y / counts(cluster_index),
-                    p.get_z / counts(cluster_index))) } )
+                (cluster_index, new Point(p.x / counts(cluster_index),
+                    p.y / counts(cluster_index),
+                    p.z / counts(cluster_index))) } )
 
             centers = averages.collect
             println("Iteration " + (iter + 1))
             for (a <- centers) {
                 val p:Point = a._2;
-                println("  Cluster " + a._1 + ", (" + p.get_x + ", " + p.get_y +
-                        ", " + p.get_z + ")")
+                println("  Cluster " + a._1 + ", (" + p.x + ", " + p.y +
+                        ", " + p.z + ")")
             }
         }
     }
@@ -114,26 +97,26 @@ object SparkKMeans {
         val input = sc.textFile(inputDir)
         val converted = input.map(line => {
             val tokens = line.split(" ")
-            val x = tokens(0).toDouble
-            val y = tokens(1).toDouble
-            val z = tokens(2).toDouble
+            val x = tokens(0).toFloat
+            val y = tokens(1).toFloat
+            val z = tokens(2).toFloat
             new Point(x, y, z) })
         converted.saveAsObjectFile(outputDir)
     }
 
     def classify(point : Point, centers : Array[(Int, Point)]) : (Int, Point) = {
-        val x = point.get_x
-        val y = point.get_y
-        val z = point.get_z
+        val x = point.x
+        val y = point.y
+        val z = point.z
 
         var closest_center = -1
         var closest_center_dist = -1.0
 
         for (c <- centers) {
             val center = c._2
-            val diffx = center.get_x - x
-            val diffy = center.get_y - y
-            val diffz = center.get_z - z
+            val diffx = center.x - x
+            val diffy = center.y - y
+            val diffz = center.z - z
             val dist = sqrt(pow(diffx, 2) + pow(diffy, 2) + pow(diffz, 2))
 
             if (closest_center == -1 || dist < closest_center_dist) {
