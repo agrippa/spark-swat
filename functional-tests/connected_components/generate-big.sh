20d9cd9ddddeeebbc00c0fb1b31c9e9bbf2889bc
Max Grossman <jmaxg3@gmail.com>
2015-08-07 07:09:35 -0500

    add big tests for connected components

---
+++ functional-tests/connected_components/generate-big.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# usage: GenerateInput output-links-dir n-output-links-files ndocs mean-nlinks nlinks-range output-docs-file
+DATA_DIR=$SPARK_DATA/connected_components
+mkdir -p $DATA_DIR
+mkdir -p $DATA_DIR/links
+rm -f $DATA_DIR/links/*
+
+# scala -classpath ./target/sparkconnectedcomponents-0.0.0.jar GenerateInput \
+#           $DATA_DIR/links 1000 10000000 300000000 $DATA_DIR/info
+scala -classpath ./target/sparkconnectedcomponents-0.0.0.jar GenerateInput \
+          $DATA_DIR/links 1000 100000 300000 $DATA_DIR/info
---
+++ functional-tests/connected_components/run-big.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $# != 1 ]]; then
+    echo 'usage: run.sh run|run-cl|check'
+    exit 1
+fi
+
+spark-submit --class SparkConnectedComponents \
+        --jars ${SWAT_HOME}/swat/target/swat-1.0-SNAPSHOT.jar,${APARAPI_HOME}/com.amd.aparapi/dist/aparapi.jar,${ASM_HOME}/lib/asm-5.0.3.jar,${ASM_HOME}/lib/asm-util-5.0.3.jar \
+        --master spark://localhost:7077 \
+        ${SWAT_HOME}/functional-tests/connected_components/target/sparkconnectedcomponents-0.0.0.jar \
+        $1 hdfs://$(hostname):54310/converted-links $SPARK_DATA/connected_components/info
