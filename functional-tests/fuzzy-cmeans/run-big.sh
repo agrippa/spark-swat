bee5aa245e6b3a2e4dc6781eac487b804a695bdc
Max Grossman <jmaxg3@gmail.com>
2015-08-16 19:13:17 -0500

    arguments that seem to make performance more reproducable

--- functional-tests/fuzzy-cmeans/run-big.sh
+++ functional-tests/fuzzy-cmeans/run-big.sh
@@ -11,6 +11,7 @@ fi
 
 spark-submit --class SparkFuzzyCMeans \
         --jars ${SWAT_HOME}/swat/target/swat-1.0-SNAPSHOT.jar,${APARAPI_HOME}/com.amd.aparapi/dist/aparapi.jar,${ASM_HOME}/lib/asm-5.0.3.jar,${ASM_HOME}/lib/asm-util-5.0.3.jar \
+        --conf "spark.executor.extraJavaOptions=-XX:GCTimeRatio=19" \
         --master spark://localhost:7077 \
         ${SWAT_HOME}/functional-tests/fuzzy-cmeans/target/sparkfuzzycmeans-0.0.0.jar \
         run 80 $1 hdfs://$(hostname):54310/converted $2
