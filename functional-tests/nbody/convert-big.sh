5a181d1cd4a7fe516e85ba168e4fd2fad7999a8a
Max Grossman <jmaxg3@gmail.com>
2015-08-20 09:30:45 -0500

    update with nbody results

--- functional-tests/nbody/convert-big.sh
+++ functional-tests/nbody/convert-big.sh
@@ -9,5 +9,6 @@ ${HADOOP_HOME}/bin/hdfs dfs -mkdir /input
 ${HADOOP_HOME}/bin/hdfs dfs -put /scratch/jmg3/spark-inputs/nbody/points/input.* /input/
 
 spark-submit --class SparkNBody --master spark://localhost:7077 \
+        --jars ${SWAT_HOME}/swat/target/swat-1.0-SNAPSHOT.jar,${APARAPI_HOME}/com.amd.aparapi/dist/aparapi.jar,${ASM_HOME}/lib/asm-5.0.3.jar,${ASM_HOME}/lib/asm-util-5.0.3.jar \
         ${SWAT_HOME}/functional-tests/nbody/target/sparknbody-0.0.0.jar convert \
         hdfs://$(hostname):54310/input hdfs://$(hostname):54310/converted
--- functional-tests/run-all.sh
+++ functional-tests/run-all.sh
@@ -3,67 +3,120 @@
 set -e
 
 OUTPUT=perf.log
+echo "" > $OUTPUT # clear it
 
-# connected_components
-cd connected_components
-./convert-big.sh
-./run-big.sh run &> spark.log
-./run-big.sh run-cl &> swat.log
-CC_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
-CC_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
-CC_SPEEDUP=$(echo $CC_SPARK_TIME / $CC_SWAT_TIME | bc -l)
-cd ..
+TESTNAME=
 
-# fuzzy cmeans
-cd fuzzy-cmeans
-./convert-big.sh
-./run-big.sh 3 false &> spark.log
-./run-big.sh 3 true &> swat.log
-FUZZY_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
-FUZZY_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
-FUZZY_SPEEDUP=$(echo $FUZZY_SPARK_TIME / $FUZZY_SWAT_TIME | bc -l)
-cd ..
+while getopts "t:h" opt; do
+    case $opt in
+        t)
+            TESTNAME=$OPTARG
+            ;;
+        h)
+            echo 'usage: run-all.sh [-t test]'
+            echo '    where test can be:'
+            echo '        connected_components'
+            echo '        fuzzy'
+            echo '        kmeans'
+            echo '        pagerank'
+            echo '        nbody'
+            exit 1
+            ;;
+        \?)
+            echo "unrecognized option -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "option -$OPTARG requires an argument" >&2
+            exit 1
+            ;;
+    esac
+done
 
-# kmeans
-cd kmeans
-./convert-big.sh
-./run-big.sh 3 false &> spark.log
-./run-big.sh 3 true &> swat.log
-KMEANS_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
-KMEANS_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
-KMEANS_SPEEDUP=$(echo $KMEANS_SPARK_TIME / $KMEANS_SWAT_TIME | bc -l)
-cd ..
+if [[ -z $TESTNAME || $TESTNAME == "connected_components" ]]; then
+    # connected_components
+    cd connected_components
+    ./convert-big.sh
+    ./run-big.sh run &> spark.log
+    ./run-big.sh run-cl &> swat.log
+    CC_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
+    CC_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
+    CC_SPEEDUP=$(echo $CC_SPARK_TIME / $CC_SWAT_TIME | bc -l)
+    cd ..
+    echo ===== Connected Components ===== >> $OUTPUT
+    echo Spark : $CC_SPARK_TIME >> $OUTPUT
+    echo SWAT : $CC_SWAT_TIME >> $OUTPUT
+    echo Speedup : $CC_SPEEDUP >> $OUTPUT
+    echo >> $OUTPUT
+fi
 
-# pagerank
-cd pagerank
-./convert-big.sh
-./run-big.sh 3 false &> spark.log
-./run-big.sh 3 true &> swat.log
-PAGERANK_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
-PAGERANK_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
-PAGERANK_SPEEDUP=$(echo $PAGERANK_SPARK_TIME / $PAGERANK_SWAT_TIME | bc -l)
-cd ..
+if [[ -z $TESTNAME || $TESTNAME == "fuzzy" ]]; then
+    # fuzzy cmeans
+    cd fuzzy-cmeans
+    ./convert-big.sh
+    ./run-big.sh 3 false &> spark.log
+    ./run-big.sh 3 true &> swat.log
+    FUZZY_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
+    FUZZY_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
+    FUZZY_SPEEDUP=$(echo $FUZZY_SPARK_TIME / $FUZZY_SWAT_TIME | bc -l)
+    cd ..
+    echo ===== Fuzzy CMeans ===== >> $OUTPUT
+    echo Spark : $FUZZY_SPARK_TIME >> $OUTPUT
+    echo SWAT : $FUZZY_SWAT_TIME >> $OUTPUT
+    echo Speedup : $FUZZY_SPEEDUP >> $OUTPUT
+    echo >> $OUTPUT
+fi
 
-echo ===== Connected Components ===== > $OUTPUT
-echo Spark : $CC_SPARK_TIME >> $OUTPUT
-echo SWAT : $CC_SWAT_TIME >> $OUTPUT
-echo Speedup : $CC_SPEEDUP >> $OUTPUT
-echo >> $OUTPUT
-echo ===== Fuzzy CMeans ===== >> $OUTPUT
-echo Spark : $FUZZY_SPARK_TIME >> $OUTPUT
-echo SWAT : $FUZZY_SWAT_TIME >> $OUTPUT
-echo Speedup : $FUZZY_SPEEDUP >> $OUTPUT
-echo >> $OUTPUT
-echo ===== KMeans ===== >> $OUTPUT
-echo Spark : $KMEANS_SPARK_TIME >> $OUTPUT
-echo SWAT : $KMEANS_SWAT_TIME >> $OUTPUT
-echo Speedup : $KMEANS_SPEEDUP >> $OUTPUT
-echo >> $OUTPUT
-echo ===== PageRank ===== >> $OUTPUT
-echo Spark : $PAGERANK_SPARK_TIME >> $OUTPUT
-echo SWAT : $PAGERANK_SWAT_TIME >> $OUTPUT
-echo Speedup : $PAGERANK_SPEEDUP >> $OUTPUT
-echo >> $OUTPUT
+if [[ -z $TESTNAME || $TESTNAME == "kmeans" ]]; then
+    # kmeans
+    cd kmeans
+    ./convert-big.sh
+    ./run-big.sh 3 false &> spark.log
+    ./run-big.sh 3 true &> swat.log
+    KMEANS_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
+    KMEANS_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
+    KMEANS_SPEEDUP=$(echo $KMEANS_SPARK_TIME / $KMEANS_SWAT_TIME | bc -l)
+    cd ..
+    echo ===== KMeans ===== >> $OUTPUT
+    echo Spark : $KMEANS_SPARK_TIME >> $OUTPUT
+    echo SWAT : $KMEANS_SWAT_TIME >> $OUTPUT
+    echo Speedup : $KMEANS_SPEEDUP >> $OUTPUT
+    echo >> $OUTPUT
+fi
+
+if [[ -z $TESTNAME || $TESTNAME == "pagerank" ]]; then
+    # pagerank
+    cd pagerank
+    ./convert-big.sh
+    ./run-big.sh 3 false &> spark.log
+    ./run-big.sh 3 true &> swat.log
+    PAGERANK_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
+    PAGERANK_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
+    PAGERANK_SPEEDUP=$(echo $PAGERANK_SPARK_TIME / $PAGERANK_SWAT_TIME | bc -l)
+    cd ..
+    echo ===== PageRank ===== >> $OUTPUT
+    echo Spark : $PAGERANK_SPARK_TIME >> $OUTPUT
+    echo SWAT : $PAGERANK_SWAT_TIME >> $OUTPUT
+    echo Speedup : $PAGERANK_SPEEDUP >> $OUTPUT
+    echo >> $OUTPUT
+fi
+
+if [[ -z $TESTNAME || $TESTNAME == "nbody" ]]; then
+    # nbody
+    cd nbody
+    ./convert-big.sh
+    ./run-big.sh 3 false &> spark.log
+    ./run-big.sh 3 true &> swat.log
+    NBODY_SPARK_TIME=$(cat spark.log | grep "Overall time" | awk '{ print $4 }')
+    NBODY_SWAT_TIME=$(cat swat.log | grep "Overall time" | awk '{ print $4 }')
+    NBODY_SPEEDUP=$(echo $NBODY_SPARK_TIME / $NBODY_SWAT_TIME | bc -l)
+    cd ..
+    echo ===== NBody ===== >> $OUTPUT
+    echo Spark : $NBODY_SPARK_TIME >> $OUTPUT
+    echo SWAT : $NBODY_SWAT_TIME >> $OUTPUT
+    echo Speedup : $NBODY_SPEEDUP >> $OUTPUT
+    echo >> $OUTPUT
+fi
 
 if [[ -f $OUTPUT.old ]]; then
     cat $OUTPUT
