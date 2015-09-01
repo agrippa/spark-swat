48b6eb835ade7b39483f0e568165641fa03ddeae
Max Grossman <jmaxg3@gmail.com>
2015-09-01 10:05:13 -0500

    check spark home from test script

--- swat/test_translator.sh
+++ swat/test_translator.sh
@@ -2,6 +2,10 @@ if [[ "x$SCALA_HOME" == "x" ]]; then
   echo SCALA_HOME must be set
   exit 1
 fi
+if [[ "x$SPARK_HOME" == "x" ]]; then
+  echo SPARK_HOME must be set
+  exit 1
+fi
 
 JARS=./target/test-classes:${APARAPI_HOME}/com.amd.aparapi/dist/aparapi.jar:./target/swat-1.0-SNAPSHOT.jar
 
