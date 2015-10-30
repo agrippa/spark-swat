2e47a4c728616d215321e9c78624e8f9142bb99c
Max Grossman <jmaxg3@gmail.com>
2015-10-30 09:44:09 -0500

    add mean computation

--- logs/overall/median-all.sh
+++ logs/overall/median-all.sh
@@ -1,9 +1,13 @@
 for dir in $(ls); do
     if [[ $dir != median-all.sh ]]; then
-        SPARK=$(cat $dir/spark | grep "overall\|Overall" | grep time | \
+        SPARK_MEDIAN=$(cat $dir/spark | grep "overall\|Overall" | grep time | \
                 awk '{ print $4 }' | python ~/median.py);
-        SWAT=$(cat $dir/swat |  grep "overall\|Overall" | grep time | \
+        SWAT_MEDIAN=$(cat $dir/swat |  grep "overall\|Overall" | grep time | \
                 awk '{ print $4 }' | python ~/median.py);
-        echo $dir $SPARK $SWAT $(echo $SPARK / $SWAT | bc -l);
+        SPARK_MEAN=$(cat $dir/spark | grep "overall\|Overall" | grep time | \
+                awk '{ print $4 }' | python ~/mean.py);
+        SWAT_MEAN=$(cat $dir/swat |  grep "overall\|Overall" | grep time | \
+                awk '{ print $4 }' | python ~/mean.py);
+        echo $dir $SPARK_MEDIAN $SWAT_MEDIAN $(echo $SPARK_MEDIAN / $SWAT_MEDIAN | bc -l) $(echo $SPARK_MEAN / $SWAT_MEAN | bc -l)
     fi
 done
