f8ab3c94b629698075aed152f9639ef3ee016ff7
Max Grossman <jmaxg3@gmail.com>
2015-11-02 13:47:02 -0600

    fix up results

--- logs/scalability/median-all.sh
+++ logs/scalability/median-all.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+NEXECUTORS=8
+
 for dir in $(ls); do
-    if [[ $dir != median-all.sh && $dir != extract_stage_times.sh ]]; then
-        SPARK_MEDIAN=$(cat $dir/spark | grep "overall\|Overall" | grep time | \
+    if [[ $dir != median-all.sh && $dir != extract_stage_times.sh && dir != scalability.sh ]]; then
+        SPARK_MEDIAN=$(cat $dir/$NEXECUTORS/spark | grep "overall\|Overall" | grep time | \
                 awk '{ print $4 }' | python ~/median.py);
-        SWAT_MEDIAN=$(cat $dir/swat |  grep "overall\|Overall" | grep time | \
+        SWAT_MEDIAN=$(cat $dir/$NEXECUTORS/swat |  grep "overall\|Overall" | grep time | \
                 awk '{ print $4 }' | python ~/median.py);
-        SPARK_MEAN=$(cat $dir/spark | grep "overall\|Overall" | grep time | \
+        SPARK_MEAN=$(cat $dir/$NEXECUTORS/spark | grep "overall\|Overall" | grep time | \
                 awk '{ print $4 }' | python ~/mean.py);
-        SWAT_MEAN=$(cat $dir/swat |  grep "overall\|Overall" | grep time | \
+        SWAT_MEAN=$(cat $dir/$NEXECUTORS/swat |  grep "overall\|Overall" | grep time | \
                 awk '{ print $4 }' | python ~/mean.py);
         echo -e "$dir\t$SPARK_MEDIAN\t$SWAT_MEDIAN\t$(echo $SPARK_MEDIAN / $SWAT_MEDIAN | bc -l)\t$(echo $SPARK_MEAN / $SWAT_MEAN | bc -l)"
     fi
