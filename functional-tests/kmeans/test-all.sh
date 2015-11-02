d250c072d745756647cf7096402ab3738ccf8ae8
Max Grossman <jmaxg3@gmail.com>
2015-11-02 11:52:27 -0600

    kmeans 4 nodes results

--- functional-tests/kmeans/test-all.sh
+++ functional-tests/kmeans/test-all.sh
@@ -10,11 +10,11 @@ NEXECUTORS=$(cat $SPARK_HOME/conf/slaves | wc -l)
 SPARK_LOG_FILE=$SWAT_HOME/logs/scalability/$APP/$NEXECUTORS/spark
 SWAT_LOG_FILE=$SWAT_HOME/logs/scalability/$APP/$NEXECUTORS/swat
 
-echo "" > $SPARK_LOG_FILE
-echo "" > $SWAT_LOG_FILE
+# echo "" > $SPARK_LOG_FILE
+# echo "" > $SWAT_LOG_FILE
 
-for i in {1..3}; do
+for i in {1..1}; do
     # usage: run.sh niters ncenters use-swat? heaps-per-device n-inputs n-outputs
-    ./run-census.sh 1 2000 false 2 2 2 >> $SPARK_LOG_FILE 2>&1
+# ./run-census.sh 1 2000 false 2 2 2 >> $SPARK_LOG_FILE 2>&1
     ./run-census.sh 1 2000 true 2 2 2 >> $SWAT_LOG_FILE 2>&1
 done
--- functional-tests/pagerank/test-all.sh
+++ functional-tests/pagerank/test-all.sh
@@ -10,10 +10,10 @@ NEXECUTORS=$(cat $SPARK_HOME/conf/slaves | wc -l)
 SPARK_LOG_FILE=$SWAT_HOME/logs/scalability/$APP/$NEXECUTORS/spark
 SWAT_LOG_FILE=$SWAT_HOME/logs/scalability/$APP/$NEXECUTORS/swat
 
-echo "" > $SPARK_LOG_FILE
-echo "" > $SWAT_LOG_FILE
+# echo "" > $SPARK_LOG_FILE
+# echo "" > $SWAT_LOG_FILE
 
-for i in {1..5}; do
+for i in {1..2}; do
     ./run-hyperlink-graph.sh true 1 2 2 2 >> $SWAT_LOG_FILE 2>&1
-    ./run-hyperlink-graph.sh false 1 2 2 2 >> $SPARK_LOG_FILE 2>&1
+    # ./run-hyperlink-graph.sh false 1 2 2 2 >> $SPARK_LOG_FILE 2>&1
 done
