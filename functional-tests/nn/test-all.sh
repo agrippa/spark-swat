a43abc2fd912ab08654b3cfb63eae781db758943
Max Grossman <jmaxg3@gmail.com>
2015-11-02 03:33:23 -0600

    kmeans 8 node

--- functional-tests/nn/test-all.sh
+++ functional-tests/nn/test-all.sh
@@ -13,7 +13,7 @@ SWAT_LOG_FILE=$SWAT_HOME/logs/scalability/$APP/$NEXECUTORS/swat
 echo "" > $SPARK_LOG_FILE
 echo "" > $SWAT_LOG_FILE
 
-for i in {1..3}; do
+for i in {1..4}; do
     # usage: run.sh use-swat? iters n-inputs n-outputs heaps-per-device
     ${HOME}/spark-swat/functional-tests/nn/run-imagenet.sh true 1 2 2 2 >> $SWAT_LOG_FILE 2>&1
     ${HOME}/spark-swat/functional-tests/nn/run-imagenet.sh false 1 2 2 2 >> $SPARK_LOG_FILE 2>&1
