29b87cb4b953f440559b715c280303550cb87fdd
Max Grossman <jmaxg3@gmail.com>
2015-12-23 07:03:10 -0600

    array input example

---
+++ functional-tests/array-input/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# != 1 && $# != 2 ]]; then
+    echo "usage run.sh cmd [use-swat?]"
+    echo "  where cmd is run or check"
+    echo "  if cmd is run, the next argument must be use-swat?. otherwise it must be empty"
+    exit 1
+fi
+CMD=$1
+
+if [[ $CMD != "run" && $CMD != "check" ]]; then
+    echo "Invalid command"
+    exit 1
+fi
+
+spark-submit --class SparkSimple \
+        --jars ${SWAT_HOME}/swat/target/swat-1.0-SNAPSHOT.jar,${APARAPI_HOME}/com.amd.aparapi/dist/aparapi.jar,${ASM_HOME}/lib/asm-5.0.3.jar,${ASM_HOME}/lib/asm-util-5.0.3.jar \
+        --master spark://localhost:7077 \
+        ${SWAT_HOME}/functional-tests/array-input/target/array_input-0.0.0.jar \
+        $CMD $2 hdfs://$(hostname):54310/converted
---
+++ functional-tests/array-input/src/main/scala/array-input/GenerateInput.scala
@@ -0,0 +1,39 @@
+import java.io._
+
+object GenerateInput {
+    def main(args : Array[String]) {
+        if (args.length != 5) {
+            println("usage: GenerateInput output-dir n-output-files " +
+                    "n-vectors-per-file avg-vec-length vec-length-range")
+            return;
+        }
+
+        val outputDir = args(0)
+        val nOutputFiles = args(1).toInt
+        val pointsPerFile = args(2).toInt
+        val avgLength = args(3).toInt
+        val lengthRange = args(4).toInt
+        assert(lengthRange < avgLength)
+
+        val r = new scala.util.Random
+        val range = 100.0
+
+        for (f <- 0 until nOutputFiles) {
+            val writer = new PrintWriter(new File(outputDir + "/input." + f))
+
+            for (p <- 0 until pointsPerFile) {
+
+                var length = r.nextInt(lengthRange * 2)
+                length = length - lengthRange
+                length = length + avgLength
+                assert(length > 0)
+
+                for (i <- 0 until length) {
+                    writer.write(r.nextDouble * range + " ")
+                }
+                writer.write("\n")
+            }
+            writer.close
+        }
+    }
+}
