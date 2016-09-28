bf285d95465a674d7f9955fabac68cdb97cb594e
Max Grossman <jmaxg3@gmail.com>
2016-09-27 23:17:19 -0500

    update cuda tests for davinci

--- dataset-transformations/hyperlinkgraph/3.docranks_again.sh
+++ dataset-transformations/hyperlinkgraph/3.docranks_again.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-# ${HADOOP_HOME}/bin/hdfs dfs -rm -f -r /renormalized-links
+${HADOOP_HOME}/bin/hdfs dfs -rm -f -r /renormalized-links
 ${HADOOP_HOME}/bin/hdfs dfs -rm -f -r /doc-ranks
 
-# ${HADOOP_HOME}/bin/hdfs dfs -mkdir /renormalized-links
-# ${HADOOP_HOME}/bin/hdfs dfs -put \
-#         $SPARK_DATASETS/hyperlinkgraph/3.renormalize/part* /renormalized-links/
+${HADOOP_HOME}/bin/hdfs dfs -mkdir /renormalized-links
+${HADOOP_HOME}/bin/hdfs dfs -put \
+        $SPARK_DATASETS/hyperlinkgraph/3.renormalize/part* /renormalized-links/
 
 spark-submit --class DocRankGenerator \
         --jars ${SWAT_HOME}/swat/target/swat-1.0-SNAPSHOT.jar,${APARAPI_SWAT}/com.amd.aparapi/dist/aparapi.jar,${ASM_HOME}/lib/asm-5.0.3.jar,${ASM_HOME}/lib/asm-util-5.0.3.jar \
--- dataset-transformations/hyperlinkgraph/3.renormalize.sh
+++ dataset-transformations/hyperlinkgraph/3.renormalize.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# ${HADOOP_HOME}/bin/hdfs dfs -rm -f -r /filtered-links
+${HADOOP_HOME}/bin/hdfs dfs -rm -f -r /filtered-links
 ${HADOOP_HOME}/bin/hdfs dfs -rm -f -r /renormalized-links
 
-# ${HADOOP_HOME}/bin/hdfs dfs -mkdir /filtered-links
-# ${HADOOP_HOME}/bin/hdfs dfs -put $SPARK_DATASETS/hyperlinkgraph/2.filter/part* /filtered-links/
+${HADOOP_HOME}/bin/hdfs dfs -mkdir /filtered-links
+${HADOOP_HOME}/bin/hdfs dfs -put $SPARK_DATASETS/hyperlinkgraph/2.filter/part* /filtered-links/
 
 spark-submit --class HyperlinkRenormalizer \
         --jars ${SWAT_HOME}/swat/target/swat-1.0-SNAPSHOT.jar,${APARAPI_SWAT}/com.amd.aparapi/dist/aparapi.jar,${ASM_HOME}/lib/asm-5.0.3.jar,${ASM_HOME}/lib/asm-util-5.0.3.jar \
